1543385d6d45dff8b43786d78792381a9d4c7c5f
FaridAiman1 <73122685+FaridAiman1@users.noreply.github.com>
2020-10-19 19:15:36 -0700

    Add files via upload

--- Driver.java
+++ Driver.java
@@ -6,12 +6,25 @@ public class Driver{
 		Fish nemo = new Fish();
 
 		nemo.Swim();
-		nemo.color = "red";
+		//nemo.color = "red";
+		nemo.SetColor("red");
 		nemo.Swim();
+		nemo.Eat();
 
-		Fish dory = new Fish();
-		dory.Swim();
-		dory.color = "yellow";
-		dory.Swim();
+
+		Shark fiery = new Shark();
+		fiery.Eat();
+		fiery.Swim();
+		fiery.SetSharpTeeth(130);
+		fiery.Eat();
+
+		//Fish dory = new Fish();
+		//dory.Swim();
+		//dory.color = "yellow";
+		//dory.SetColor("yellow");
+		//dory.Swim();
+
+		//Aquirium fancyAquirium = new Aquirium();
+		//fancyAquirium.fillfish();
 	}
 }
--- Fish.java
+++ Fish.java
@@ -6,4 +6,13 @@ public class Fish{
 	public void Swim (){
 		System.out.println(color + "fish swim");
 	}
+
+	public void SetColor(String warna) {
+		color = warna;
+	}
+
+	public String GetColor(){
+		return color;
+			
+		}
 }
